e46a119a2fe1aef380a85a81039c2debbf5e2440
Oliver Filla <ocl@hotmail.de>
2018-05-24 20:05:38 +0200

    print cowsay of hard-coded string

--- 3/03_cowsay.py
+++ 3/03_cowsay.py
@@ -3,3 +3,26 @@ Created on May 24, 2018
 
 @author: of
 '''
+
+cow = """
+        \\   ^__^
+         \\  (oo)\\_______
+            (__)\\       )\\/\\
+                ||----w |
+                ||     ||
+"""
+
+
+speech_bubble_top_symbol = """_"""
+speech_bubble_bottom_symbol = """-"""
+
+def main():
+    tosay = "Python rocks!"
+    length = len(tosay)
+    speech = '  ' + speech_bubble_top_symbol * length + "\n"
+    speech += '< ' + tosay + " >"
+    speech += '\n  ' + speech_bubble_bottom_symbol * length 
+    print speech + cow
+
+if __name__ == "__main__":
+    main()
