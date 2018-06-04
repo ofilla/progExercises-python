bcf4ea5656adb6c3ad3894ef75bed13eb8370f69
Oliver Filla <oliver.filla@gendis.de>
2018-06-04 15:31:51 +0200

    solved part 2

--- 02_kassenbon_part2.py
+++ 02_kassenbon_part2.py
@@ -1,7 +1,6 @@
 # input parameter
-products = ['Wurst', 'Kaese', 'Brot', 'DVD']
-numbers_to_buy = [1, 1, 1, 2]
-wallet = {50: 2}
+item = 'DVD'
+wallet = {50: 1}
 
 products_prices = {
     'Wurst': 4.2,
@@ -9,14 +8,27 @@ products_prices = {
     'Brot': 2.1,
     'DVD': 12
 }
+
+# auto-buy
+wallet_total = 0
+for bill in wallet:
+    wallet_total += bill * wallet[bill]
+
+nr_to_buy = 0
+price_per_item = products_prices[item]
+price = 0
+while price + price_per_item < wallet_total:
+    # add item to basket
+    nr_to_buy += 1
+    price += price_per_item
     
+basket = [(nr_to_buy, item)]
+
 # init
 preceeding_spaces = " "*24 # 24 = 3*\t
 maxprice = 0
 total = 0
 bon = []
-
-basket = zip(numbers_to_buy, products)
 
 def pricestring(price):
     return '{0:6.2f} EUR'.format(price)
@@ -42,14 +54,22 @@ print 'Gesamt                  ' + pricestring(total)
 # what is given?
 given = 0
 rest = total
-while given < total:
+while rest > 0:
     # bills only, coins are not implemented
     for bill in (500, 200, 100, 50, 20, 10, 5):
         if wallet.get(bill, 0) > 0:
             # wallet contains at least one of this kind of bill
             given += bill # pay
+            wallet[bill] -= 1
             rest = total - given
-                
-print 'Gegeben                 ' + pricestring(given)
-print
-print 'Zurueck                 ' + pricestring(- rest)
+            break
+    if bill == 5:
+        # wallet is empty
+        print
+        print "not enough money to buy all items"
+        break
+
+if rest <= 0:
+    print 'Gegeben                 ' + pricestring(given)
+    print
+    print 'Zurueck                 ' + pricestring(- rest)
